48646a3a65617cbd615cbea5bc768de1714c3fd7
Augustin734 <augustin.viemont@epitech.com>
2025-10-24 15:45:29 +0200

    softskills

--- src/jsx/Skills.jsx
+++ src/jsx/Skills.jsx
@@ -7,6 +7,13 @@ import docker from '../images/docker.JPG';
 import node from '../images/node.JPG';
 import pg from '../images/PG.JPG';
 
+import avengers from'../images/avengers.JPG/';
+import lebronjames from '../images/lebronjames.JPG';
+import thisisfine from '../images/thisisfine.JPG';
+import handshake from '../images/handshake.JPG';
+import stonks from '../images/stonks.JPG';
+import anglais from '../images/anglais.JPG';
+
 function Skills() {
   return (
   <div>
@@ -53,21 +60,28 @@ function Skills() {
       <div className="HardSkills">
         <div className="Skills">
           <p>Travail d'équipe</p>
+          <img className="logoSK" src={avengers}/>
         </div>
         <div className="Skills">
           <p>Gestion de la pression</p>
+          <img className="logoSK" src={lebronjames}/>
         </div>
         <div className="Skills">
           <p>Gestion du stress</p>
+          <img className="logoSK" src={thisisfine}/>
         </div>
         <div className="Skills">
           <p>Gestion client</p>
+          <img className="logoSK" src={handshake}/>
         </div>
         <div className="Skills">
           <p>Vente</p>
+          <br/>
+          <img className="logoSK" src={stonks}/>
         </div>
         <div className="Skills">
           <p>Anglais B2</p>
+          <img className="logoSK" src={anglais}/>
         </div>
       </div>
     <div className='fade'/>
